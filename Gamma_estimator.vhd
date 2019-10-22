bce2bf7f4eb9af3a3d6ace7f3177e538528b69ce
Pedram Babakhani <35693865+PedramBabakhani@users.noreply.github.com>
2019-10-22 14:37:41 +0200

    Add files via upload

---
+++ Gamma_estimator.vhd
@@ -0,0 +1,45 @@
+----------------------------------------------------------------------------------
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use ieee.numeric_std.all;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
+
+
+entity Gamma_estimator is
+port(  input: in std_logic_vector(7 downto 0);
+       nrst : in std_logic;
+		 clk : in std_logic;
+		 enable : out std_logic;
+		 gamma: out std_logic_vector(7 downto 0) 
+		 );
+end Gamma_estimator;
+
+architecture Behavioral of Gamma_estimator is
+
+signal sum_of_brightness : std_logic_vector(15 downto 0) :="0000000000000000";
+signal average_of_brightness : std_logic_vector(7 downto 0);
+signal counter : integer:=0;
+signal flag : std_logic;
+begin
+process(clk)
+variable gamma_temp : std_logic_vector(7 downto 0);
+begin
+  if(nrst = '1') then
+    counter <= 0;
+	 sum_of_brightness <= "0000000000000000"; 
+	 average_of_brightness <="00000000";
+	 flag <= '0';
+  elsif(rising_edge(clk)) then
+	 sum_of_brightness <= input + sum_of_brightness;
+	 counter <= counter+ 1;
+	 if (counter = 256) then
+	 counter <= 0;
+	 sum_of_brightness <= "0000000000000000";
+    gamma <= sum_of_brightness(15 downto 8);
+    enable <= '1';	 
+	 end if;
+ end if;
+end process;
+end Behavioral;
+
---
+++ automatic_gamma_correction.vhd
@@ -0,0 +1,84 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    18:36:19 01/15/2016 
+-- Design Name: 
+-- Module Name:    automatic_gamma_correction - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity automatic_gamma_correction is
+ port( input: in std_logic_vector(7 downto 0);
+       nrst : in std_logic;
+		 clk : in std_logic;
+       output : out std_logic_vector (7 downto 0)		 
+		 );
+end automatic_gamma_correction;
+
+
+architecture Behavioral of automatic_gamma_correction is
+
+COMPONENT gamma_correction IS
+port(  input: in std_logic_vector(7 downto 0);
+       nrst : in std_logic;
+		 clk : in std_logic;
+		 enable : in std_logic;
+		 gamma: in std_logic_vector(7 downto 0);
+       output : out std_logic_vector (7 downto 0)		 
+		 );
+END COMPONENT;
+
+COMPONENT Gamma_estimator is
+port(  input: in std_logic_vector(7 downto 0);
+       nrst : in std_logic;
+		 clk : in std_logic;
+		 enable : out std_logic;
+		 gamma: out std_logic_vector(7 downto 0) 
+		 );
+END COMPONENT;
+
+
+signal gamma : std_logic_vector(7 downto 0);
+signal enable : std_logic;
+begin
+
+gamma_estimator_module : Gamma_estimator PORT MAP ( input       => input,
+                                             nrst => nrst,
+                                             clk => clk,
+															enable => enable,
+                                             gamma   => gamma  
+                                             );
+															
+gamma_correction_module : gamma_correction PORT MAP ( input => input,
+                                               nrst => nrst,
+                                               clk => clk,
+															  enable => enable,
+                                               gamma   => gamma,
+															  output => output
+                                             );
+
+
+end Behavioral;
+
